a4800603e38d46c5b1fb12970fc0bcbe4506b42d
Leandro Kaul <leandrokaul@hotmail.com>
2021-02-16 01:01:11 -0300

    Se conecta con firebase permitiendo realizar una compra, guardando el documento en la nube con los datos solicitados y mostrando al usuario el id de la compra

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,28 +1,48 @@
 import React from "react";
+import firebase from "firebase/app"
 import {useCartContext} from "../CartContext/CartContext"
 import {useEffect, useState} from "react"
 import {Link} from "react-router-dom"
 import "./Cart.css"
+import {getFirestore} from "../../firebase/index"
 const Cart =()=>{
     const {removeCarrito,carrito}=useCartContext();
-    
-    let total=0;
-    const item={
-        id:"",
-        title:"",
-        price:0
-    }
-    console.log(total);
-    const buyer={
+    const [order,setOrder]=useState({})
+    const [orderId,setOrderId]=useState([])
+
+    let totalCompra=0;
+
+    const buyerInfo={
         name:"Leandro",
         phone:"1140404040",
         email:"leandro@gmail.com"
     }
     carrito.map((item)=>{
-
-        total=item.item.price*item.quantity+total;       
-        
+        /*const aux={id:"",title:"",price:0}
+        aux.id=item.item.id;
+        aux.price=item.item.price;
+        aux.title=item.item.title;*/
+        totalCompra=item.item.price*item.quantity+totalCompra;         
     })
+    
+    useEffect(()=>{
+        setOrder({
+            buyer:buyerInfo,
+            items:carrito,
+            date: firebase.firestore.Timestamp.fromDate(new Date()),
+            total:totalCompra
+        })
+    },[totalCompra])
+    
+    const handleCompra =()=>{
+        const db=getFirestore()
+        const orderDb=db.collection('orders')
+        orderDb.add(order)
+            .then(({id})=>{
+                setOrderId(id)
+            })
+            .catch((e)=>console.log("ocurrio un error:",e))
+    }
     
     return(
 
@@ -30,7 +50,8 @@ const Cart =()=>{
             
         {
            <div> {carrito.length===0 ? <div><h1>Aun no agregaste productos al carrito</h1> 
-                                       <Link to='/'><button className="btn btn-success">Volver al inicio</button></Link></div> :
+                                       <Link to='/'><button className="btn btn-success">Volver al inicio</button></Link></div> 
+                                    :
             carrito.map(item=>{
                 return(
                     <div className="cartItem mx-auto d-flex flex-row col-10 align-items-center" key={item.item.id}>
@@ -41,9 +62,15 @@ const Cart =()=>{
                     </div>
                 )
             })
+            
             }
-            <h1>TOTAL:{total} </h1>
-            <button>Finalizar compra</button>
+            {orderId.length!=0 && <div><h1>Id de la compra:{orderId}</h1></div>}
+            
+            {totalCompra>0 &&  <div>
+                <h1>TOTAL:{totalCompra} </h1>
+                <button onClick={handleCompra} >Finalizar compra</button>
+                </div>}
+           
             </div>
           
         }
